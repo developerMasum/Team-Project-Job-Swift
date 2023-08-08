8639aa8bdff7170a60d60c26f46110d8dde36aeb
saifulaija <saifulaija2023@gmail.com>
2023-08-09 03:11:30 +0600

    Demo page implemented

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import HrPlatform from '../../components/HrPlatform/HrPlatform';
 
 const Home = () => {
     return (
         <div>
           <h2>Home Page : Welcome Guys !! </h2>  
+          <HrPlatform></HrPlatform>
+          
         </div>
     );
 };
--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import FreeDemo from "../Pages/FreeDemo/FreeDemo";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home />
+        },
+        {
+          path:'free-demo',
+          element:<FreeDemo></FreeDemo>
         }
       ]
     },
