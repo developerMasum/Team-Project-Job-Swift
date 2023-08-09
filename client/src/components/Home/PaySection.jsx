1414f5b3faede17b8b61fc9a5fb2f2dd06053faa
developerMasum <developer.masum2001@gmail.com>
2023-08-10 00:39:38 +0600

    fixed styles in homepage

--- client/src/components/Home/PaySection.jsx
+++ client/src/components/Home/PaySection.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BiSolidChevronRight } from "react-icons/bi";
+import { FaChevronRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const PaySection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center justify-center ml-44">
-      <div className="md:ml-20 md:mr-20">
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 px-10 md:px-0   md:ml-96   items-center justify-items-end">
+      <div className=" ">
         <h2 className="text-2xl md:text-3xl font-semibold text-primary mb-2">
           Payroll, Time & Benefits
         </h2>
@@ -16,20 +16,19 @@ const PaySection = () => {
           your employees.
         </p>
 
-        <Link to={"/"}>
-          {" "}
-          <p className="text-primary flex gap-1 text-lg md:text-xl font-semibold items-center mt-4 md:mt-10">
-            Learn More About Payroll, Time & Benefits{" "}
-            <span>
-              <BiSolidChevronRight className="w-6 h-6 text-primary" />
+        <Link to={"/"} className="">
+          <h3 className="text-primary text-lg md:text-xl font-semibold mt-4 md:mt-10 ">
+            Learn More About Payroll, Time & Benefits
+            <span className="ml-2">
+              <FaChevronRight className="inline-block text-primary" />
             </span>
-          </p>
+          </h3>
         </Link>
       </div>
-      <div className="mt-6 md:mt-0 w-2/3">
+      <div className="mt-6 md:mt-0   ">
         <img
           src="https://www.bamboohr.com/media_1a3f05063d0c65ac5f1e19b54dca44ef785738332.png?width=2000&format=webply&optimize=medium"
-          className="w-full h-auto"
+          className="w-96 h-auto rounded-2xl "
           alt=""
         />
       </div>
