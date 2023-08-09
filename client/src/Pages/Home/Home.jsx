b0029972d30e91d477a019da26aecf4b075b87f8
saifulaija <saifulaija2023@gmail.com>
2023-08-09 20:03:07 +0600

    final check done

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
 import PaySection from '../../components/Home/PaySection';
 import HrPlatform from '../../components/Home/HrPlatform';
-=======
-import HrPlatform from '../../components/HrPlatform/HrPlatform';
->>>>>>> 8639aa8bdff7170a60d60c26f46110d8dde36aeb
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
           {/* important !!!  here please everyone render his section one by one from components-> home-> your section name  */}
           <PaySection />
           <HrPlatform></HrPlatform>
 
          
-=======
-          <h2>Home Page : Welcome Guys !! </h2>  
-          <HrPlatform></HrPlatform>
-          
->>>>>>> 8639aa8bdff7170a60d60c26f46110d8dde36aeb
         </div>
     );
 };
--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -2,11 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
-<<<<<<< HEAD
 import DemoPage from "../Pages/DemoPage/DemoPage";
-=======
-import FreeDemo from "../Pages/FreeDemo/FreeDemo";
->>>>>>> 8639aa8bdff7170a60d60c26f46110d8dde36aeb
 
 const router = createBrowserRouter([
     {
@@ -19,13 +15,8 @@ const router = createBrowserRouter([
             element: <Home />
         },
         {
-<<<<<<< HEAD
           path:'demo-page',
           element:<DemoPage></DemoPage>
-=======
-          path:'free-demo',
-          element:<FreeDemo></FreeDemo>
->>>>>>> 8639aa8bdff7170a60d60c26f46110d8dde36aeb
         }
       ]
     },
