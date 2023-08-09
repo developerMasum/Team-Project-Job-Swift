59ebff14f7a55a83bd8016837a44240ba182720b
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-08-09 21:30:05 +0600

    Hiring sectin From ashk Faysal

---
+++ client/src/Pages/Home/Hiring.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hiring = () => {
+  return (
+    <>
+      <div>
+        <p>image</p>
+      </div>
+      <div>
+        <p> text section</p>
+      </div>
+    </>
+  );
+};
+
+export default Hiring;
