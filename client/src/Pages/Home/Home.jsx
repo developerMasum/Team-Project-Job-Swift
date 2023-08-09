30af8ae3f230c70cf5f12fa9e9891f27d82101c8
developerMasum <developer.masum2001@gmail.com>
2023-08-10 00:46:29 +0600

    config solved

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import HrDataReporting from '../../Components/Home/HrDataReporting/HrDataReporting';
-import PaySection from '../../components/Home/PaySection';
-import HrPlatform from '../../components/Home/HrPlatform';
-import Banner from '../../Components/Home/Banner/Banner';
-
+import React from "react";
+import HrDataReporting from "../../Components/Home/HrDataReporting/HrDataReporting";
+import PaySection from "../../components/Home/PaySection";
+import HrPlatform from "../../components/Home/HrPlatform";
+import Banner from "../../Components/Home/Banner/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-          <HrDataReporting />
-          {/* important !!!  here please everyone render his section one by one from components-> home-> your section name  */}
-          <HrPlatform />
-      <div className='mt-12'>
+  return (
+    <div>
+      <Banner></Banner>
       <HrDataReporting />
+      {/* important !!!  here please everyone render his section one by one from components-> home-> your section name  */}
+      <HrPlatform />
+      <div className="mt-12">
+        <HrDataReporting />
       </div>
-         
-         <div className='mt-12'>
-         <PaySection />
-         </div>
 
-         
-        </div>
-    );
+      <div className="mt-12">
+        <PaySection />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
