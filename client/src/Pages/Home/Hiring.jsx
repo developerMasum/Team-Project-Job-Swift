eb9083f644ea0d544fc4b0cfd1087cb27e92a585
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-08-10 08:29:26 +0600

    added an image and right side text

--- client/src/Pages/Home/Hiring.jsx
+++ client/src/Pages/Home/Hiring.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-
+import hiringImage from "../../../public/hiring.png"
 const Hiring = () => {
   return (
-    <>
-      <div>
-        <p>image</p>
+    <div>
+      <div className="md:flex">
+        <img src={hiringImage} alt="" />
       </div>
       <div>
-        <p> text section</p>
+        <h1 className="text-6xl">Hiring & Onboarding</h1>
+        <p>
+          BambooHR helps you quickly find, hire, and onboard the best talent.
+          With our powerful applicant tracking system and proactive onboarding
+          tasks, you can create a compelling candidate experience and better
+          first days for new hires.
+        </p>
+
+        <a href="#">Learn More About Hiring & Onboarding</a>
       </div>
-    </>
+    </div>
   );
 };
 
